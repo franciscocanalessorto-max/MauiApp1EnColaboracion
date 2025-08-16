11b0df7d1cc558925723e04cbddf491899f2bffb
ESFE-AGAPE-INDEL26 <ESFE-AGAPE-INDEL26@LUIS>
2025-08-16 11:02:13 -0600

    Agregar archivos de proyecto.

---
+++ MauiApp1EnColaboracion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp1EnColaboracion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApp1EnColaboracion/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1EnColaboracion
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
